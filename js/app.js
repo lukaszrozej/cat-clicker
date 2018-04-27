0b81c7b645b849503b95d137d854025a4e19c778
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-04-27 09:22:34 +0200

    feat: Add third cat

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const body = document.querySelector('body');
 const cats = [
   new Cat('Claudio', 'img/claudio.jpg'),
   new Cat('Igor', 'img/igor.jpg'),
+  new Cat('Ottorino', 'img/ottorino.jpg'),
 ];
 
 
